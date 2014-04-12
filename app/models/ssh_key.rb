b8c7d65d4f449681a0a6db6856bb870ef1b57d31
flower-pot <flower-pot@flowerpot.io>
2014-04-12 20:56:32 +0200

    add validation of ssh key and calculation of fingerprint

--- app/models/ssh_key.rb
+++ app/models/ssh_key.rb
@@ -1,3 +1,24 @@
 class SshKey < ActiveRecord::Base
+  validate :validate_key
+  validates_presence_of :title
+  validates_presence_of :content
+
+  before_save :set_fingerprint
+
   belongs_to :user
+
+  def set_fingerprint
+    self.fingerprint = SSHKey.fingerprint self.content
+  end
+
+  def validate_key
+    if !valid_key?
+      errors.add(:key, "is not a valid ssh public key")
+    end
+  end
+
+  def valid_key?
+    SSHKey.valid_ssh_public_key? self.content
+  end
+
 end
