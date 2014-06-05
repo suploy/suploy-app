a6219aed1b74e354f5cbbb3f580a801d7d04f086
flower-pot <flower-pot@flowerpot.io>
2014-06-05 16:28:20 +0200

    fix parameters for linking images

--- app/workers/recreate_with_db_worker.rb
+++ app/workers/recreate_with_db_worker.rb
@@ -2,6 +2,8 @@ class RecreateWithDbWorker
   include Sidekiq::Worker
 
   def perform(app_name, db_container_name)
-    Scli::Container.create_app_container_with_db(app_name, "", db_container_name)
+    puts app_name
+    puts db_container_name
+    Scli::Container.create_app_container_with_db(app_name+"image", "", db_container_name)
   end
 end
